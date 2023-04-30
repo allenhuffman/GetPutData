9ec78bb71d2dbdf2222cb7f816d88df6252bf73a
Allen Huffman <alsplace@pobox.com>
2023-04-30 14:48:19 -0500

    Create GetPutValues.h

---
+++ GetPutValues.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+// Prototypes
+void getData(uint8_t **ptr, void *dataPtr, unsigned int dataSize);
+uint8_t getU8(uint8_t **ptr);
+uint16_t getU16(uint8_t **ptr);
+uint32_t getU32(uint8_t **ptr);
+float getFloat(uint8_t **ptr);
+double getDouble(uint8_t **ptr);
+
+void putData(uint8_t **ptr, void *dataPtr, unsigned int dataSize);
+void putU8(uint8_t **ptr, uint8_t value);
+void putU16(uint8_t **ptr, uint16_t value);
+void putU32(uint8_t **ptr, uint32_t value);
+void putFloat(uint8_t **ptr, float value);
+void putDouble(uint8_t **ptr, double value);
